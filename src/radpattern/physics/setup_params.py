af851f2a67af5297af0f2eb7ac94fe8c616dbf50
Radek <radekvr@gmail.com>
2026-05-13 14:14:17 +0200

    naming convention

--- src/radpattern/physics/setup_params.py
+++ src/radpattern/physics/setup_params.py
@@ -258,10 +258,10 @@ class SetupParams:
 
         return (
             f"{self.regime.atoms}_{self.regime.buffer_pressure_Torr}Torr"
-            f"{self.regime.signal_fwhm_diamter_m}SDia_{self.regime.control_fwhm_diameter_m}Cdia_"
+            f"{int(self.regime.signal_fwhm_diameter_m * 1e6)}SDia_{int(self.regime.control_fwhm_diameter_m * 1e6) }Cdia"
             f"_simT{self.sim.sim_time_us}us"
             f"_nt{self.sim.time_divisions}"
-            f"{self.sim.n_mc}runs"
+            f"_{self.sim.n_mc}runs"
             f"_{h}"
         )
 
