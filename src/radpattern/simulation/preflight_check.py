726133cf69fab249a2633079e934dc600eb448e6
Radek Vasicek <radekvr@gmail.com>
2026-07-01 00:10:11 +0200

    Def error fix. Diff_code added correctly

--- src/radpattern/simulation/preflight_check.py
+++ src/radpattern/simulation/preflight_check.py
@@ -86,7 +86,7 @@ def pre_simulation_warnings(objs, theta0):
     # The code set a dt_max limit of dt_max = 0.1 * min( exp.char_size) **2 / (2.0 * Diff_coef) 
     # Then for dt bigger it divides into smaller steps. 
     max_step_fraction = 0.1
-    dt_max_lim = (max_step_fraction * cloud.char_size) ** 2 / (2.0 *Diff_coef) 
+    dt_max_lim = (max_step_fraction * cloud.char_size) ** 2 / (2.0 * exp.diffusion_coeff_code) 
     n_sub = max(1, int(np.ceil( dt_max / dt_max_lim) ))
     if n_sub >= 2_000: 
         log.warning("max_n_sub= %d. This run is effectively very expensive. Consider more time points or shorter sim_time.") 
